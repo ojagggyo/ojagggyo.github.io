84bdef2a038505cad6383e464610febc20999da6
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2021-01-16 14:03:45 +0900

    Update steemitapi05.js

--- steemitapi05.js
+++ steemitapi05.js
@@ -1,32 +1,32 @@
-
-
-    async function Sample(error) {
-        if(error){
-            // throwしているため、この値がrejectされる
-            throw new Error('★Error★');
-        }
-        else{
-            // returnしているため、この値がresolveされる
-            return '★Success★';
-        }   
+async function Sample(error) {
+    if(error){
+        // throwしているため、この値がrejectされる
+        throw new Error('★Error★');
     }
+    else{
+        // returnしているため、この値がresolveされる
+        return '★Success★';
+    }   
+}
 
-    Sample(true)
-        .then(value => {
-            console.log(value);
-            $("#Sample1").text(value);
-        })
-        .catch(err => {
-            console.log(err);
-            $("#Sample1").text(err);
-        });
+Sample(true)
+    .then(value => {
+        console.log(value);
+        $("#Sample1").text(value);
+    })
+    .catch(err => {
+        console.log(err);
+        $("#Sample1").text(err);
+        $('#Sample1').css('color','red');
+    });
 
-    Sample(false)
-        .then(value => {
-            console.log(value);
-            $("#Sample2").text(value);
-        })
-        .catch(err => {
-            console.log(err);
-            $("#Sample2").text(err);
-        });
+Sample(false)
+    .then(value => {
+        console.log(value);
+        $("#Sample2").text(value);
+    })
+    .catch(err => {
+        console.log(err);
+        $("#Sample2").text(err);
+        $('#Sample2').css('color','red');
+    });
