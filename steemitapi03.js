172fabe8888b79cc6bbcc61bc52b56295fb894b0
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2021-01-17 21:22:17 +0900

    Create steemitapi03.js

---
+++ steemitapi03.js
@@ -0,0 +1,28 @@
+let username;
+function saveUserName(){
+    username = window.location.hash;// #username
+    if (username == null || username.trim().length == 0){
+        username = "#steemitblog";
+    }
+
+    username = username.substr(1);//#を取る
+    username = decodeURI(username).trim();//デコード、トリム
+}
+
+async function getAccountsAsync(){
+    let accounts = await steem.api.getAccountsAsync([username]);
+    console.log(accounts);
+    if(accounts.length == 0) {return;}
+    let account = accounts[0];
+    let vp = account.voting_power + (10000 * ((new Date - new Date(account.last_vote_time + "Z")) / 1000) / 432000);
+    var text = "Voting Powerは " + (vp/100).toFixed(4) + " %です。";
+    $("#votingPower").text(text);
+}
+
+window.onload = function() {
+    steem.api.setOptions({url: 'https://api.steemit.com'});
+    saveUserName();
+    getAccountsAsync();
+
+    setInterval(getAccountsAsync, 5000);
+};
