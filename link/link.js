bfcbb2bc051ca2deb6b227bbbb10bbc15188cde7
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2021-04-21 13:16:36 +0900

    Create link.js

---
+++ link/link.js
@@ -0,0 +1,15 @@
+  
+let username;
+function saveUserName(){
+    username = window.location.hash;// #username
+    if (username == null || username.trim().length == 0){
+        username = "#steemitblog";
+    }
+
+    username = username.substr(1);//#を取る
+    username = decodeURI(username).trim();//デコード、トリム   
+}
+
+window.onload = function() {
+    saveUserName();
+};
