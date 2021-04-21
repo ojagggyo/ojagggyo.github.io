7cee3f0013faba576a051cc4a34045d0cfe41a67
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2021-04-21 12:19:47 +0900

    Update link.js

--- archive/link.js
+++ archive/link.js
@@ -10,6 +10,7 @@ function saveUserName(){
     
 }
 
+/*
 var log = function(){
     bbb = $("tbody").html().replace(/@/g, '@'+username);
     bbb = bbb.replace(/a=/g, 'a='+username);
@@ -19,8 +20,26 @@ var log = function(){
    
     $("tbody").html(bbb);
 };
+*/
+
+function link(){
+
+    //$('#lll').html('<h1>リンク</h1>');
+    
+    bbb = $("#link1").html();
+    bbb = bbb.replace(/@/g, '@'+username);
+    bbb = bbb.replace(/a=/g, 'a='+username);
+    bbb = bbb.replace(/steemworld.org/g, 'steemworld.org/@'+username);
+    bbb = bbb.replace(/steemscan.com/g, 'steemscan.com/account/'+username);
+    bbb = bbb.replace(/steemdb.io/g, 'steemdb.io/@'+username);
+    $("#link1").html(bbb);
+    
+    //w = $("a").html();
+    //$("a").html(w.replace(/steemdb.io/g, 'steemdb.io/@'+username));
+}
 
 window.onload = function() {
     saveUserName();
-    setTimeout(log, 0);
+    //setTimeout(log, 0);
+    link();
 };
