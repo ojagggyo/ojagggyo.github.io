0aefb7c0eb0b3646db507e7ac16991d2f823b558
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2021-11-01 03:16:01 +0900

    Update myutil.js

--- steemitapi/myutil.js
+++ steemitapi/myutil.js
@@ -22,9 +22,7 @@ function getAccounts(username){
     console.log(accounts);
     console.log(accounts.length);
     if(accounts.length == 0) {return;}
-    let account = accounts[0];
-    console.log(account);
-    return account;
+    return= accounts[0];
   }).catch(err => {
     console.log(err);
   });
