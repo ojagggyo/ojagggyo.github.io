f606e37ee208979fb8aee8515ac72391529e8204
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2021-11-03 01:50:58 +0900

    Update pcr_textarea.js

--- chart/pcr_textarea.js
+++ chart/pcr_textarea.js
@@ -1,1 +1,45 @@
-###
+let myChart;
+
+function clickBtn() {
+  document.getElementById("text1").value;
+}
+  
+function clickBtn() {
+  let t1 = document.getElementById("text1").value;
+  let csv = t1.split(/\n/);//改行で分割する
+  console.log(csv);
+
+  //2次元を2個の1次元に変換する。
+  var labels = [];
+  var datas = [];
+  for(var i=0;i<csv.length;i=i+1){
+    row = csv[i].split(/,/);
+    labels.push(row[0]);
+    datas.push(row[1]);
+  }
+  console.log(labels);
+  console.log(datas);
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: '新規陽性者数',
+      backgroundColor: 'rgb(51, 221, 204)',
+      borderColor: 'rgb(51, 221, 204)',
+      data: datas,
+    }]
+  };
+
+  // === include 'setup' then 'config' above ===
+  const config = {
+  type: 'bar',
+    data: data,
+    options: {}
+  };
+
+  if(myChart != null){
+    myChart.destroy();
+  }
+  myChart = new Chart(
+      document.getElementById('myChart'), config);
+}
