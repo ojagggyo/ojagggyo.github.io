de2510a83cf460dcb3768f171ea2243e1d250838
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2021-11-04 02:42:01 +0900

    Update ownerhistory.js

--- steemitapi/ownerhistory.js
+++ steemitapi/ownerhistory.js
@@ -1,7 +1,7 @@
 steem.api.setOptions({url: 'https://api.steemit.com'});
 
 async function getOwnerHistory(){  
-  wait steem.api.getOwnerHistory('support-jp', function(err, result) {
+  await steem.api.getOwnerHistory('support-jp', function(err, result) {
      console.log('☆');
     console.log(err, result);
     return result;
