040c9b9483e1c44a5122dd978ca2ef1b7c934a91
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2021-11-04 10:50:15 +0900

    Update ownerhistory.js

--- steemitapi/ownerhistory.js
+++ steemitapi/ownerhistory.js
@@ -1,36 +1,28 @@
+steem.api.setOptions({url: 'https://api.steemit.com'});    
+
+async function getOwnerHistoryAsync(accountname){  
+  let ownerHistory = await steem.api.getOwnerHistoryAsync(accountname);
+  console.log('☆☆');
+  console.log(ownerHistory);
+  return ownerHistory;
+}
+            
 function clickBtn() {
   let t1 = document.getElementById("text1").value;
   let csv = t1.split(/\n/);//改行で分割する
-
-  //csv形式を2個の1次元に変換する。
-  let labels = [];
-  let datas = [];
+  
   for(var i=0;i<csv.length;i=i+1){
-    row = csv[i].split(/,/);//カンマで分割する
-    labels.push(row[0]);
-    datas.push(row[1]);
+    getOwnerHistoryAsync(csv[i]).then(result => {
+      console.log('★★');
+      console.log(result);
+      //console.log(result[0].last_valid_time);
+      //console.log(result[0].previous_owner_authority);
+      //console.log(result[0].previous_owner_authority.key_auths);
+      //console.log(result[0].previous_owner_authority.key_auths.length);
+      //onsole.log(result[0].previous_owner_authority.key_auths[0]);
+
+      $("#text").text(result[0].last_valid_time);
+    }).catch(err => {
+    });
   }
-
-  const data = {
-    labels: labels,
-    datasets: [{
-      label: '新規陽性者数',
-      backgroundColor: 'rgb(51, 221, 204)',
-      borderColor: 'rgb(51, 221, 204)',
-      data: datas,
-    }]
-  };
-
-  // === include 'setup' then 'config' above ===
-  const config = {
-  type: 'bar',
-    data: data,
-    options: {}
-  };
-
-  if(myChart != null){
-    myChart.destroy();
-  }
-  myChart = new Chart(
-      document.getElementById('myChart'), config);
 }
