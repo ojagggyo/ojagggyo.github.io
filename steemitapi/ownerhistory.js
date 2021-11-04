41f25eb23203739244f8da9ae9506ac8e1b58e5a
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2021-11-04 10:43:57 +0900

    Update ownerhistory.js

--- steemitapi/ownerhistory.js
+++ steemitapi/ownerhistory.js
@@ -1,8 +1,36 @@
-steem.api.setOptions({url: 'https://api.steemit.com'});
+function clickBtn() {
+  let t1 = document.getElementById("text1").value;
+  let csv = t1.split(/\n/);//改行で分割する
 
-function getOwnerHistory(){  
-  let ownerHistory = steem.api.getOwnerHistory('support-jp');
-  console.log('☆');
-  console.log(ownerHistory);
-  return ownerHistory;
+  //csv形式を2個の1次元に変換する。
+  let labels = [];
+  let datas = [];
+  for(var i=0;i<csv.length;i=i+1){
+    row = csv[i].split(/,/);//カンマで分割する
+    labels.push(row[0]);
+    datas.push(row[1]);
+  }
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: '新規陽性者数',
+      backgroundColor: 'rgb(51, 221, 204)',
+      borderColor: 'rgb(51, 221, 204)',
+      data: datas,
+    }]
+  };
+
+  // === include 'setup' then 'config' above ===
+  const config = {
+  type: 'bar',
+    data: data,
+    options: {}
+  };
+
+  if(myChart != null){
+    myChart.destroy();
+  }
+  myChart = new Chart(
+      document.getElementById('myChart'), config);
 }
