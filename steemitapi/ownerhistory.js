c006a121cd6f18593c914d42a857655b597c3e33
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2021-11-04 10:59:51 +0900

    Update ownerhistory.js

--- steemitapi/ownerhistory.js
+++ steemitapi/ownerhistory.js
@@ -11,7 +11,15 @@ function clickBtn() {
   let t1 = document.getElementById("text1").value;
   let csv = t1.split(/\n/);//改行で分割する
   
+  let html = '<table>';
+  html = html + '<table>';
+  
   for(var i=0;i<csv.length;i=i+1){
+    html = html + '<tr>';
+    html = html + '<th>';
+    html = html + csv[i];
+    html = html + '<th>';
+    
     getOwnerHistoryAsync(csv[i]).then(result => {
       console.log('★★');
       console.log(result);
@@ -21,8 +29,13 @@ function clickBtn() {
       //console.log(result[0].previous_owner_authority.key_auths.length);
       //onsole.log(result[0].previous_owner_authority.key_auths[0]);
 
-      $("#text").text(result[0].last_valid_time);
+      html = html + '<td>';
+      html = html + result[0].last_valid_time);
+      html = html + '</td>';
     }).catch(err => {
     });
+    
+    html = html + '</tr>';
   }
+  html = html + '</table>';
 }
