c144354051ef60ff5a094a6c3559c9cacc6af0cc
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2021-12-24 18:56:50 +0900

    Update getaccounthistory.0.6.js

--- steemitapi/getaccounthistory.0.6.js
+++ steemitapi/getaccounthistory.0.6.js
@@ -1,1 +1,35 @@
+function donokuraimae(date){
+	date1 = new Date(date);
+	date1.setHours(date1.getHours() + 9);
+	var now = new Date();
+	sa = now - date1;
+	if(sa >= 86400000){return Math.floor(sa / 86400000)+'日前';}
+	if(sa >= 3600000){return Math.floor(sa / 3600000)+'時間前';}
+	if(sa >= 60000){return Math.floor(sa / 60000)+'分前';}
+	if(sa >= 1000){return Math.floor(sa / 1000)+'秒前';}
+	return 'たった今';
+}
 
+function vestToSteem(vest){
+	return  steem.formatter.vestToSteem(
+		vest, 
+		globalProperties.total_vesting_shares, 
+		globalProperties.total_vesting_fund_steem)
+}
+	
+function ellipsis(s){
+	return s.length > 40 ? (s).slice(0,40)+"…" : s;
+}
+
+function clickBtn(days){
+	aaa(days).then(result => {		
+		makeTable(result);
+	}).catch(err => {
+		console.log(err);
+	});
+}
+
+function inputChange(event){
+    let username = document.getElementById("username").value;
+    jdenticon.update("#identicon", username);
+}
