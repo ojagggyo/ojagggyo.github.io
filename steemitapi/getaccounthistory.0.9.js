7e3abded280d816b988f8ebad88e8b8d67b968b3
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2021-12-28 21:25:25 +0900

    Update getaccounthistory.0.9.js

--- steemitapi/getaccounthistory.0.9.js
+++ steemitapi/getaccounthistory.0.9.js
@@ -1,1 +1,62 @@
 
+function donokuraimae(date){
+	date1 = new Date(date);
+	date1.setHours(date1.getHours() + 9);
+	var now = new Date();
+	sa = now - date1;
+	if(sa >= 86400000){return Math.floor(sa / 86400000)+'日前';}
+	if(sa >= 3600000){return Math.floor(sa / 3600000)+'時間前';}
+	if(sa >= 60000){return Math.floor(sa / 60000)+'分前';}
+	if(sa >= 1000){return Math.floor(sa / 1000)+'秒前';}
+	return 'たった今';
+}
+
+function vestToSteem(vest){
+	return  steem.formatter.vestToSteem(
+		vest, 
+		globalProperties.total_vesting_shares, 
+		globalProperties.total_vesting_fund_steem)
+}
+	
+function ellipsis(s){
+	return s.length > 40 ? (s).slice(0,40)+"…" : s;
+}
+
+function getUserName(){
+  let hash = window.location.hash;// #username
+  if (hash == null || hash.trim().length == 0){
+	  return "";
+  }
+  hash = hash.substr(1);//#を取る
+  hash = decodeURI(hash).trim();//デコード、トリム]
+  return hash;
+}
+
+function clickBtn(days){
+	emoji();
+	document.getElementById("progress").innerText = "";
+	document.getElementById("author_reward").innerText = "";
+	document.getElementById("curation_reward").innerText = "";
+	document.getElementById("comment_benefactor_reward").innerText = "";
+	document.getElementById("text").innerText = "";
+	
+	total_count = {};
+	total_sbd_payout = {};
+	total_steem_payout = {};
+	total_vesting_payout = {};
+	total_sp_payout = {};
+	
+	window.location.hash = '#' + document.getElementById("username").value;
+	aaa(days).then(result => {
+		document.getElementById("text").innerText = 'processing...';
+		makeTable(result);
+	}).catch(err => {
+		document.getElementById("text").innerText = err;
+		console.log(err);
+		alart(err);
+	});
+}
+
+function inputChange(event){
+    jdenticon.update("#identicon", document.getElementById("username").value);
+}
