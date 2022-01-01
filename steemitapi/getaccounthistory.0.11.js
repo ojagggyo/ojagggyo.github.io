26bf167c64d3641035c108bca6e6a4659ba912a5
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2022-01-01 17:38:41 +0900

    Update getaccounthistory.0.11.js

--- steemitapi/getaccounthistory.0.11.js
+++ steemitapi/getaccounthistory.0.11.js
@@ -177,6 +177,128 @@ function votingpower(username){
 	});
 }
 
+//reputation
+function log10(str) {
+    const leadingDigits = parseInt(str.substring(0, 4));
+    const log = Math.log(leadingDigits) / Math.LN10 + 0.00000001;
+    const n = str.length - 1;
+    return n + (log - parseInt(log));
+}
+
+function repLog10(str) {
+    let rep = String(str);
+    const neg = rep.charAt(0) === '-';
+    rep = neg ? rep.substring(1) : rep;
+    let out = log10(rep);
+    if (isNaN(out)) out = 0;
+    out = Math.max(out - 9, 0); // @ -9, $0.50 earned is approx magnitude 1
+    out = (neg ? -1 : 1) * out;
+    out = out * 9 + 25; // 9 points per magnitude. center at 25  
+    return out;
+};
+
+async function getReputation(username){
+	return new Promise((resolve, reject) => {
+		steem.api.getAccounts([username], function(err, response) {
+		    if (err) reject(err);
+		    resolve(repLog10(response[0].reputation));
+		});
+	});
+}
+
+ function reputation(username){
+	getReputation(username).then(result => {
+		document.getElementById("reputation").text = result.toFixed(3);
+	}).catch(err => {
+		console.log(err);
+	});	
+}
+	
+//age
+//27.3217
+async function getAge(username){
+	return new Promise((resolve, reject) => {
+		steem.api.getAccounts([username], function(err, response) {
+			if (err) reject(err);
+			date1 = new Date(response[0].created);
+			date1.setHours(date1.getHours() + 9);
+			var now = new Date();
+			sa = now - date1;
+			
+			resolve({
+				moons: sa / 86400000 / 27.3217, 
+				days: sa / 86400000, 
+				earths: sa / 86400000 / 365.242
+			});
+		});
+	});
+}
+
+ function age(username){
+	getAge(username).then(result => {
+		if(result.moons < 1){
+			document.getElementById("age").text = result.days.toFixed(3) + ' days';
+		}else if(result.earths < 1){
+			document.getElementById("age").text = result.moons.toFixed(3) + ' moons';
+		}else{
+			document.getElementById("age").text = result.earths.toFixed(3) + ' earths';
+		}
+
+	}).catch(err => {
+		console.log(err);
+	});	
+}
+	
+//reward
+let total_count = {};
+let total_sbd_payout = {};
+let total_steem_payout = {};
+let total_vesting_payout = {};
+let total_sp_payout = {};
+function getReward(record){
+	const username = document.getElementById("username").value
+	let sbd_payout = 0;
+	let steem_payout = 0;
+	let vesting_payout = 0;
+	let op = record[1].op[0];
+	if(op == "comment_benefactor_reward" && record[1].op[1].benefactor == username){
+		sbd_payout = parseFloat(record[1].op[1].sbd_payout);
+		steem_payout = parseFloat(record[1].op[1].steem_payout);
+		vesting_payout = parseFloat(record[1].op[1].vesting_payout);
+	}else if(op == "author_reward"){
+		sbd_payout = parseFloat(record[1].op[1].sbd_payout);
+		steem_payout = parseFloat(record[1].op[1].steem_payout);
+		vesting_payout = parseFloat(record[1].op[1].vesting_payout);
+	}else if(op == "curation_reward"){
+		sbd_payout = 0;
+		steem_payout = 0;
+		vesting_payout = parseFloat(record[1].op[1].reward);
+	}else {
+		return false;
+	}
+	
+	total_sp_payout[op] = steem.formatter.vestToSteem(
+			total_vesting_payout[op], 
+			globalProperties.total_vesting_shares, 
+			globalProperties.total_vesting_fund_steem)
+	
+	if(total_count[op] === void 0){
+		total_count[op] = 1;
+		total_sbd_payout[op] = sbd_payout;
+		total_steem_payout[op] = steem_payout;
+		total_vesting_payout[op] = vesting_payout;
+		total_sp_payout[op] = vestToSteem(vesting_payout);
+	}else{
+		total_count[op] += 1;
+		total_sbd_payout[op] += sbd_payout;
+		total_steem_payout[op] += steem_payout;
+		total_vesting_payout[op] += vesting_payout;
+		total_sp_payout[op] += vestToSteem(vesting_payout);
+	}
+	return true;
+}
+
+
 // ---------- ----------
 function clickBtn(days){
 	emoji();
