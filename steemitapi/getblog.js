36f74ae81ea9747b96fb208eb012621ea7d47976
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2022-01-02 22:17:30 +0900

    Update getblog.js

--- steemitapi/getblog.js
+++ steemitapi/getblog.js
@@ -71,4 +71,4 @@ function makeTable(records){
 steem.api.setOptions({url: 'https://api.steemit.com'});
 if(!('DOMAIN' in window)){DOMAIN = 'steemit.com';}
 //const GETBLOG_LIMIT = 500;
-const GETBLOG_LIMIT = 100;
+const GETBLOG_LIMIT = 400;
