fcc005184cf8291a7b96583586370ead045830f6
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2025-03-15 17:47:27 +0900

    Update block_age.js

--- steemitapi/block_age.js
+++ steemitapi/block_age.js
@@ -1,27 +1,22 @@
 steem.api.setOptions({url: 'https://api.steememory.com'});
 
 async function main(){
-    let a = await steem.api.getDynamicGlobalPropertiesAsync();
-    let current_sbd_supply = parseFloat(a.current_sbd_supply);
-    console.log(current_sbd_supply);
-    $("#current_sbd_supply").text(current_sbd_supply);
-    let virtual_supply = parseFloat(a.virtual_supply);
-    console.log(virtual_supply);
-    $("#virtual_supply").text(virtual_supply);
-    let b = await steem.api.getCurrentMedianHistoryPriceAsync();
-    let base = parseFloat(b.base);
-    let quote = parseFloat(b.quote);
-    let steem_price = base / quote;
-    console.log(steem_price);
-    $("#steem_price").text('$'+steem_price);
-    let sbddebtratio = current_sbd_supply * 100 / (virtual_supply * steem_price);
-    $("#text").text(sbddebtratio +'%');
-    let sbd_print_rates = (sbddebtratio > 10) ? 0 : (sbddebtratio < 9) ? 100 : (10 - sbddebtratio) * 100
-    $("#sbd_print_rates").text(sbd_print_rates.toFixed(1) +'%');
-    document.querySelector("#progress").value = sbd_print_rates;
+    /*
+    steem.api.getWitnessByAccount("yasu.witness", function(err, result) {
+      console.log(err, result);
+    });
+*/
+    
+    let a = await steem.api.getWitnessByAccount("yasu.witness");
+
+        console.log(a);
+/*
+document.querySelector("#progress").value = sbd_print_rates;
+
+*/
     //チャート更新
     labels.push(new Date().toLocaleString());
-    datas.push(sbddebtratio.toFixed(9) );
+    datas.push("111" );
     myChart.update();
 }
 
