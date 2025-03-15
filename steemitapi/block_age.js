4e414564b673f907ed722f9760606cd4cff079e5
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2025-03-15 18:36:45 +0900

    Update block_age.js

--- steemitapi/block_age.js
+++ steemitapi/block_age.js
@@ -8,7 +8,8 @@ async function main(){
 */
     
     let a = await steem.api.getWitnessByAccountAsync("yasu.witness");
-
+    let b = a.result.witnesses[0].last_confirmed_block_num";
+    
         console.log(a);
 /*
 document.querySelector("#progress").value = sbd_print_rates;
