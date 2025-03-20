566a12f823bb97ea67884a470f6dc8617beef60e
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2025-03-20 13:32:04 +0900

    Create jpykrw.js

---
+++ steemitapi/jpykrw.js
@@ -0,0 +1,50 @@
+steem.api.setOptions({url: 'https://api.steememory.com'});
+
+async function main(){
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow"
+    };
+    
+    fetch("https://steememory.com/rate/?source=JPY&target=KRW", requestOptions)
+      .then(
+          (response) => 
+      {
+          response.text()
+          //チャート更新
+          labels.push(new Date().toLocaleString());
+          datas.push(diff);
+          myChart.update();
+      }
+      )
+      .then(
+          (result) => console.log(result)
+      )
+      .catch((error) => console.error(error));
+}
+
+window.onload = function() {
+    main();
+    setInterval(function () {
+        main();
+    }, 10*1000);
+};
+
+let labels = [];
+let datas = [];
+const data = {
+labels: labels,
+datasets: [{
+    label: 'JPYKRW rate',
+        backgroundColor: 'rgb(51, 221, 204)',
+        borderColor: 'rgb(51, 221, 204)',
+    data: datas,
+    fill: true, 
+    backgroundColor: 'rgba(51, 221, 204, 0.2)',
+}]
+};
+// === include 'setup' then 'config' above ===
+const config = {
+    type: 'line',
+    data: data,
+};
