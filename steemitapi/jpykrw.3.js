bf3e9ede6ae4f7b40a713a1435a04cca1f7fe7b7
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2025-03-22 21:45:59 +0900

    Update and rename jpykrw.2.js to jpykrw.3.js

--- steemitapi/jpykrw.3.js
+++ steemitapi/jpykrw.3.js
@@ -12,7 +12,7 @@ async function main_0(){
     //.toISOString()
     //toLocaleDateString();
     
-    fetch("https://api.wise.com/v1/rates?source=JPY&target=KRW&from=" + from.toLocaleDateString() + "&to=" + to.toLocaleDateString() + "&group=minute", requestOptions)
+    fetch("https://api.wise.com/v1/rates?source=JPY&target=KRW&from=" + from.toLocaleDateString().replace("/","-") + "&to=" + to.toLocaleDateString().replace("/","-") + "&group=minute", requestOptions)
       .then(
           (response) => response.text())
       .then(
