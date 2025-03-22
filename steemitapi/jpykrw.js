8fa78556e17b60d95a7fa08a94b2aef55611e739
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2025-03-22 21:38:08 +0900

    Update jpykrw.js

--- steemitapi/jpykrw.js
+++ steemitapi/jpykrw.js
@@ -1,5 +1,50 @@
 steem.api.setOptions({url: 'https://api.steememory.com'});
 
+async function main_0(){
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow"
+    };
+
+    let to  = new Date();
+    let from  = new Date();
+    from.setMinutes(from.getMinutes() - 24 * 60);
+    //.toISOString()
+    //toLocaleDateString();
+    
+    fetch("https://api.wise.com/v1/rates?source=JPY&target=KRW&from=" + from.toLocaleDateString() + "&to=" + to.toLocaleDateString() + "&group=minute", requestOptions)
+      .then(
+          (response) => response.text())
+      .then(
+          (result) => {
+              console.log(jsonData);
+              //チャート更新
+            // JSONへ変換
+            // JavaScriptオブジェクトへ変換
+            let objData = JSON.parse(jsonData);
+            console.log(objData);
+              
+            for (var item in objData) {
+                let time = item.time;
+                let rate = item.rate;
+
+                 console.log(time);
+                 console.log(rate);
+
+                labels.push(new Date(item.time).toLocaleString());
+                datas.push(item.rate);
+            }
+
+            /*for (var i = 0; i < objData.length; i++) {
+                let time = objData.time[i];
+                let rate = objData.rate[i];
+            }*/
+            myChart.update();
+      }
+      )
+      .catch((error) => console.error(error));
+}
+
 async function main(){
     const requestOptions = {
       method: "GET",
@@ -24,7 +69,8 @@ async function main(){
 window.onload = function() {
     let span = new URL(window.location.href).searchParams.get('span') ?? 3;
     console.log(span);
-    main();
+    main_0();
+    //main();
     setInterval(function () {
         main();
     }, span * 60 * 1000);
