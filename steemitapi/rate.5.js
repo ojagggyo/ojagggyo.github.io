58bd8f44af60983f24a49fd725fbca3b8838ae20
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2025-04-12 11:00:45 +0900

    Update and rename rate.4.js to rate.5.js

--- steemitapi/rate.5.js
+++ steemitapi/rate.5.js
@@ -136,7 +136,7 @@ const config = {
                 if (context.tick.value == 10) {
                   return '#FF0000';
                 } else {
-                  return Chart.defaults.scale.gridLines.color;
+                  return Chart.defaults.global.defaultColor;
                 }
               },
             },
