d0e847abb3b6d99ffc344f8516d651dfe1ccd552
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2025-04-13 20:59:13 +0900

    Update and rename rate.1.js to rate.js

--- steemitapi/rate.js
+++ steemitapi/rate.js
@@ -128,17 +128,9 @@ const config = {
             grid: {
               color: function(context) {
                 return (context.tick.value > 9.999 && context.tick.value < 10.001) ? '#FF0000' : 'rgba(0, 0, 0, 0.1)';//default color
-                //if (context.tick.value > 9.999 && context.tick.value < 10.001) {
-                //  return '#FF0000';
-                //}
-                //return 'rgba(0, 0, 0, 0.1)';//default color
               },
               lineWidth: function(context) {
                 return (context.tick.value > 9.999 && context.tick.value < 10.001) ? 4 : 1;
-                //if (context.tick.value > 9.999 && context.tick.value < 10.001) {
-                //  return 4;
-                //}
-                //return 1;
               },
             },
           }
