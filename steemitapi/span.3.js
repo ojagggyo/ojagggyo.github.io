0a537ffa448675acd02d0ce3cb494ee74bfebace
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2025-04-27 17:35:48 +0900

    Update and rename span.2.js to span.3.js

--- steemitapi/span.3.js
+++ steemitapi/span.3.js
@@ -1,10 +1,10 @@
-function addA(){
+function addA(source,target){
   var texts = [ '[1 day]', '[1 week]', '[1 month]', '[3 months]', '[1 year]' ].reverse();
   var hrefs = [ '?span=day', '?span=week', '?span=month', '?span=3months', '?span=year' ].reverse();
   texts.forEach(function( elem, index ) {
     let a = document.createElement('a');
     a.text = texts[index];
-    a.href = hrefs[index];
+    a.href = hrefs[index]+'&source='+source+'&target='+target;;
     document.body.prepend(a);
   });
 }
