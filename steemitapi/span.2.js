00681c3333c740b9746ada5e97d8c44766346e58
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2025-04-27 17:26:17 +0900

    Update and rename span.1.js to span.2.js

--- steemitapi/span.2.js
+++ steemitapi/span.2.js
@@ -1,6 +1,6 @@
 function addA(){
-  var texts = [ '[1 day]', '[1 week]', '[1 month]', '[3 months]' ].reverse();
-  var hrefs = [ '?span=day', '?span=week', '?span=month', '?span=3months' ].reverse();
+  var texts = [ '[1 day]', '[1 week]', '[1 month]', '[3 months]', '[1 year]' ].reverse();
+  var hrefs = [ '?span=day', '?span=week', '?span=month', '?span=3months', '?span=year' ].reverse();
   texts.forEach(function( elem, index ) {
     let a = document.createElement('a');
     a.text = texts[index];
