b87fc8b3e3bf35f6dc6afc0a2b2fcbf905f5903e
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2025-04-29 13:47:12 +0900

    Update span.10.js

--- steemitapi/span.10.js
+++ steemitapi/span.10.js
@@ -10,7 +10,10 @@ function addA(source,target){
   let a = document.createElement('a');
   a.text = 'within ';
   document.body.prepend(a);  
-a.style.fontFamily = 'Meiryo';
+
+
+    let root = document.documentElement; //htmlのルート要素を取得
+root.style.fontFamily = 'Meiryo';
 
   
   //let root = document.documentElement; //htmlのルート要素を取得
