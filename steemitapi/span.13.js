69709098a39854cd7e4524119b77d908075e041e
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2025-05-03 20:25:26 +0900

    Update span.13.js

--- steemitapi/span.13.js
+++ steemitapi/span.13.js
@@ -1,5 +1,5 @@
 function addA(source,target){
-  var texts = [ '[1 day]', '[3 day]', '[1 week]', '[1 month]', '[3 months]', '[1 year]', '[3 years]' ].reverse();
+  var texts = [ '[1 day]', '[3 days]', '[1 week]', '[1 month]', '[3 months]', '[1 year]', '[3 years]' ].reverse();
   var hrefs = [ '?span=day', '?span=3days', '?span=week', '?span=month', '?span=3months', '?span=year', '?span=3years' ].reverse();
   texts.forEach(function( elem, index ) {
     let a = document.createElement('a');
